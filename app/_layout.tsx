eec25f709809580500ae0564b77be93305c5ad0e
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2025-04-05 14:29:23 -0300

    feat: initial setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,39 +1,41 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import { useFonts } from 'expo-font';
+import { Rowdies_400Regular } from '@expo-google-fonts/rowdies';
+import * as SplashScreen from 'expo-splash-screen';
 import 'react-native-reanimated';
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { theme } from '@/styles';
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  const [isLoaded, error] = useFonts({
+    Rowdies_400Regular: Rowdies_400Regular,
   });
 
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
+  const { colors } = theme;
 
-  if (!loaded) {
-    return null;
-  }
+  useEffect(() => {
+    if (isLoaded || error) SplashScreen.hideAsync();
+  }, [isLoaded, error]);
+
+  if (!isLoaded && !error) return null;
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
+    <>
+      <StatusBar
+        style='light'
+        backgroundColor={colors.primary.main}
+        translucent
+        networkActivityIndicatorVisible
+      />
+
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name='index' />
+        <Stack.Screen name='+not-found' />
       </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    </>
   );
 }
---
+++ styles/theme.ts
@@ -0,0 +1,74 @@
+export const theme = {
+  colors: {
+    primary: {
+      main: '#A855F7',
+      opacity: {
+        25: 'rgba(168, 85, 247, 0.25)',
+      },
+    },
+
+    secondary: {
+      main: '#C084FC',
+    },
+
+    background: {
+      main: '#F3E8FF',
+    },
+
+    pH: {
+      1: '#750000',
+      2: '#B30000',
+      3: '#D1342D',
+      4: '#E86A92',
+      5: '#B857A6',
+      6: '#8137C0',
+      7: '#5B30E0',
+      8: '#3B62F4',
+      9: '#2788D6',
+      10: '#1EAB9F',
+      11: '#29C76F',
+      12: '#6EDC4D',
+      13: '#B6E738',
+      14: '#F5E33B',
+    },
+  },
+
+  fonts: {
+    sizes: {
+      heading: 32,
+      body: 16,
+      text: 12,
+    },
+
+    families: {
+      rowdies: {
+        regular: 'Rowdies_400Regular',
+      },
+    },
+  },
+
+  borders: {
+    radius: {
+      small: 4,
+      full: 9999,
+    },
+
+    widths: {
+      small: 1,
+    },
+  },
+
+  spacings: {
+    0: 0,
+    '0.5': 2,
+    1: 4,
+    '1.5': 6,
+    2: 8,
+    '2.5': 10,
+    3: 12,
+    '3.5': 14,
+    4: 16,
+    '4.5': 18,
+    5: 20,
+  },
+} as const;
