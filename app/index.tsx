0bf07ce0a15a41ba19bc65feda4e76ae2fd4f6aa
mateusbarbosa <barbosamat16@gmail.com>
2025-04-07 10:04:11 -0300

    feat: implement main `App` component with splash screen and font loading

--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,28 @@
-export function Home() {
-  return <></>;
+import React, { useEffect, useCallback, useState } from "react";
+import * as SplashScreen from "expo-splash-screen";
+import { View } from "react-native";
+
+import { useFonts, Rowdies_400Regular } from "@expo-google-fonts/rowdies";
+import SplashScreenUI from "@/components/SplashScreen";
+
+SplashScreen.preventAutoHideAsync(); // Keep native splash screen visible
+
+export default function App() {
+  const [fontsLoaded] = useFonts({ Rowdies_400Regular });
+  const [appReady, setAppReady] = useState(false);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      setAppReady(true);
+      await SplashScreen.hideAsync(); // Hide native splash once ready
+    }
+  }, [fontsLoaded]);
+
+  useEffect(() => {
+    onLayoutRootView();
+  }, [onLayoutRootView]);
+
+  if (!fontsLoaded) return null;
+
+  return <SplashScreenUI />;
 }
