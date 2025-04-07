898178fe657ce787b9303c26a3e2bdd2cfc607b2
mateusbarbosa <barbosamat16@gmail.com>
2025-04-07 10:03:52 -0300

    feat: add `Logo` component with SVG representation

---
+++ components/Icon/logo.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Logo: React.FC = () => {
+  return (
+    <svg
+      width="128"
+      height="148"
+      viewBox="0 0 128 148"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M64 12C99.3462 12 128 40.6538 128 76C128 111.346 99.3462 140 64 140C28.6538 140 0 111.346 0 76C0 40.6538 28.6538 12 64 12ZM112 76C112 102.51 90.5098 124 64 124V28C90.5098 28 112 49.4902 112 76Z"
+        fill="#A855F7"
+      />
+    </svg>
+  );
+};
+
+export default Logo;
