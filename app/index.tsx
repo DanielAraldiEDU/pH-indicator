01bfc583353493474fcfe314119d85e290e28edf
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2025-04-13 11:10:16 -0300

    feat: add first structure of the `Home` screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,16 @@
+import { theme } from '@/styles';
+import { StyleSheet, View } from 'react-native';
+
 export default function Home() {
-  return <></>;
+  return <View style={styles.container}></View>;
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+
+    width: '100%',
+
+    padding: theme.spacings[5],
+  },
+});
