dcab1a6d1a2b31be65321384195de606749ed0d9
Jonathas Meine <jonhy.meine@gmail.com>
2025-04-13 16:32:21 -0300

    feat: add app base structure

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,28 @@
-import { StyleSheet, View } from 'react-native';
+import { ScrollView, StyleSheet, View } from 'react-native';
 
 import { theme } from '@/styles';
+import { LinearGradientBox, PhButton } from '../components';
+import Divider from '../components/Divider';
+import Footer from '../components/Footer';
+import Header from '../components/Header';
 
 export default function Home() {
-  return <View style={styles.container}></View>;
+  return <ScrollView style={styles.container}>
+    <Header />
+    <Divider variant='header'/>
+    <View style={{ flexDirection: 'row', height: 700, gap: 6 }}>
+      <View style={{ width: '15%', justifyContent: 'space-between' }}>
+        <PhButton variant='0' />
+        <PhButton variant='7' />
+        <PhButton variant='14' />
+      </View>
+      <View style={{ flexGrow: 1 }}>
+        <LinearGradientBox />
+      </View>
+    </View>
+    <Divider variant='footer'/>
+    <Footer />
+  </ScrollView>;
 }
 
 const styles = StyleSheet.create({
