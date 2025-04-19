27238933b5c90a558c23f2d38a185a873a153f88
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2025-04-19 14:42:32 -0300

    feat: add `onLayout` to the `LinearGradientBox`

--- @types/components/LinearGradientBox.ts
+++ @types/components/LinearGradientBox.ts
@@ -1,2 +1,8 @@
+import { LayoutChangeEvent } from 'react-native';
+
 export type LinearGradientColorsType = [string, string, ...string[]];
 export type LinearGradientLocationsType = [number, number, ...number[]];
+
+export interface LinearGradientBoxProps {
+  onLayout?(event: LayoutChangeEvent): void;
+}
--- components/LinearGradientBox/index.tsx
+++ components/LinearGradientBox/index.tsx
@@ -2,17 +2,29 @@ import { LinearGradient } from 'expo-linear-gradient';
 import { memo } from 'react';
 import { View } from 'react-native';
 
-import { LinearGradientColorsType, LinearGradientLocationsType } from '@/@types';
+import {
+  LinearGradientBoxProps,
+  LinearGradientColorsType,
+  LinearGradientLocationsType,
+} from '@/@types';
 import { theme } from '@/styles';
 import { styles } from './styles';
 
-function LinearGradientBox() {
+function LinearGradientBox(props: LinearGradientBoxProps) {
+  const { onLayout } = props;
+
   const colors = Object.values(theme.colors.pH) as LinearGradientColorsType;
-  const locations = Object.values(theme.colors.pHStops) as LinearGradientLocationsType;
+  const locations = Object.values(
+    theme.colors.pHStops
+  ) as LinearGradientLocationsType;
 
   return (
-    <View style={styles.container}>
-      <LinearGradient colors={colors} locations={locations} style={styles.content} />
+    <View style={styles.container} onLayout={onLayout}>
+      <LinearGradient
+        colors={colors}
+        locations={locations}
+        style={styles.content}
+      />
     </View>
   );
 }
